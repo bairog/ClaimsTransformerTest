95b99fb205cfb8fe1660ec1af6d71592a5ce32c7
bairog <44995181+bairog@users.noreply.github.com>
2021-12-24 10:42:43 +0300

    Add files via upload

---
+++ ClaimsTransformerTest/ClaimsTransformer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+
+namespace ClaimsTransformerTest
+{
+    public class ClaimsTransformer : IClaimsTransformation
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public ClaimsTransformer(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            return Task.FromResult(principal);
+        }
+    }
+}
